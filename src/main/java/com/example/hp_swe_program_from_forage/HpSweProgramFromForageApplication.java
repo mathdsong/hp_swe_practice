e4878594ffa2869ce9870f3510ed1bbf33227850
dongningsong <mathdsong@gmail.com>
2023-11-13 21:26:27 -0700

    initial commit

---
+++ src/main/java/com/example/hp_swe_program_from_forage/HpSweProgramFromForageApplication.java
@@ -0,0 +1,13 @@
+package com.example.hp_swe_program_from_forage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpSweProgramFromForageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpSweProgramFromForageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hp_swe_program_from_forage/HpSweProgramFromForageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hp_swe_program_from_forage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpSweProgramFromForageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
